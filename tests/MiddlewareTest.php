f88bf511cab6a881913226b599aada21603746a2
Hauke Ingwersen <hauke@sourceboat.com>
2019-08-12 14:11:58 +0200

    MiddlewareTest setup to protected

--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class MiddlewareTest extends TestCase
 {
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
